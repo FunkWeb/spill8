c147d2e6f5c1f2cbfa0f45bb746204a40e74beba
Hans Emil Eid <32166248+hanseei@users.noreply.github.com>
2025-10-14 13:43:15 +0200

    :tada: Opprettet Godot-prosjekt. Closes: #3
    Satte opp Godot-prosjekt (i Godot 4.5) med korrekt mappestruktur, placeholder-filer i enden av alle filstier, og font-filene lastet opp.

---
+++ noder/bondegard/bondegard_scripts/testscript.gd
@@ -0,0 +1,7 @@
+extends Node
+
+#Dette er et placeholder script!
+#Det ligger her for å vise hva slags filtype som skal ligge her,
+#samt fordi jeg ikke kan pushe opp tomme mapper til git.
+
+# -Hans
