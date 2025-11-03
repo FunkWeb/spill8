5b494803bbf7ccb02d1b6c6f2ade95272a320abe
Jonis8 <jonisjonis8@gmail.com>
2025-11-03 12:04:53 +0100

    redigert filene

--- testscene.gd
+++
@@ -1,7 +0,0 @@
-extends Node2D
-
-func _ready():
-	var dialog_scene = load("res://dialog_boks.tscn").instantiate()
-	add_child(dialog_scene)
-	dialog_scene.set_text("Hei! Dette er en test av NPC-dialogboksen.")
-	dialog_scene.show_dialog()
