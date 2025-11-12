e3e9722f72ddcaa5b1a0888e414e427d6b503ab5
edasa235 <edvardaslaksen130206@gmail.com>
2025-11-12 11:07:13 +0100

    feat: lagt till gravity og spawn egg på tilfeldige steder

--- noder/spill_eggsamling/eggsamling_scripts/fallingegg.gd
+++ noder/spill_eggsamling/eggsamling_scripts/fallingegg.gd
@@ -1,8 +1,15 @@
 extends Node2D
 
+signal egg_fallen
+
 var gravity := 300.0
 var velocity := Vector2.ZERO
+@export var fall_limit := 240.0 
 
 func _process(delta: float) -> void:
 	velocity.y += gravity * delta
 	position += velocity * delta
+
+	if position.y > fall_limit:
+		emit_signal("egg_fallen")
+		queue_free() 
