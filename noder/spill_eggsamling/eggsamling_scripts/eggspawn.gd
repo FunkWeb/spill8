252e058654248bab31f965aa505660a20b4f8243
edasa235 <edvardaslaksen130206@gmail.com>
2025-11-17 13:37:59 +0100

    refactor kurv og basket og prøvd å lage et collision funksjon

--- noder/spill_eggsamling/eggsamling_scripts/eggspawn.gd
+++ noder/spill_eggsamling/eggsamling_scripts/eggspawn.gd
@@ -3,21 +3,38 @@ extends Node2D
 @export var egg_scene: PackedScene
 @export var spawn_area_width: float = 427.0
 @export var top_y: float = -10.0
+@export var score_label_path: NodePath = "ScoreLabel"  
 
-func _ready() -> void:
+var score: int = 0
+var score_label: Label
+
+func _ready():
 	if egg_scene == null:
-		push_error("egg_scene not assigned! Drag egg.tscn into Inspector.")
+		push_error("egg_scene not assigned!")
 		return
 
-	spawn_egg()  
+	update_score_label() 
+	spawn_egg()
 
 func spawn_egg() -> void:
 	var egg = egg_scene.instantiate()
+	if egg == null:
+		push_error("Failed to instantiate egg_scene!")
+		return
 	var x_pos = randf_range(0.0, spawn_area_width)
 	egg.position = Vector2(x_pos, top_y)
 	add_child(egg)
 
 	egg.connect("egg_fallen", Callable(self, "_on_egg_fallen"))
 
+
 func _on_egg_fallen() -> void:
 	spawn_egg()
+
+func egg_caught():
+	score += 1
+	update_score_label()
+
+func update_score_label():
+	if score_label:
+		score_label.text = "Score: %d" % score
