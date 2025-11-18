e8e1eb17f2ef177bb2c9f2c8dc1340b7613cd776
Dennis <gamerdennisa@gmail.com>
2025-11-18 13:06:55 +0000

    Chore : taking more to test

--- noder/karakterer/karakter_scripts/testscript.gd
+++ noder/karakterer/karakter_scripts/testscript.gd
@@ -1,7 +1,25 @@
-extends Node
+extends CharacterBody2D
 
-#Dette er et placeholder script!
-#Det ligger her for å vise hva slags filtype som skal ligge her,
-#samt fordi jeg ikke kan pushe opp tomme mapper til git.
+class_name NPCGrunn
 
-# -Hans
+@export var npc_name: String = "Ukjent"
+@export var speed: float = 50.0
+
+var direction: Vector2 = Vector2.ZERO
+
+func _ready():
+	print("NPC klar:", npc_name)
+	if has_node("Label"):
+		$Label.text = npc_name
+
+	if has_node("AnimatedSprite2D"):
+		var sprite_node = $AnimatedSprite2D
+		var frames = load("res://assets/grafikk/sprites/sprite_frames.tres")
+		if frames:
+			sprite_node.frames = frames
+			sprite_node.animation = "default"
+			sprite_node.play()
+
+func _physics_process(_delta):
+	velocity = direction * speed
+	move_and_slide()
