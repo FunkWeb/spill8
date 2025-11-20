a88b77e3432b7d6db5a8bc0855b1add4daa2b0d4
1haut <ozioma.marshall.anike@funkweb.org>
2025-11-20 15:21:54 +0100

    feat: legg til inngangsobjekt
    - inngangen har: redigerbar variabel, kollisjonsboks, knappeinput som bytter scene innenfor inngangsområdet

---
+++ noder/ui_objekter/inngang/inngang.gd
@@ -0,0 +1,17 @@
+extends Area2D
+
+@export var next_scene: String = "res://scener/egg_collection_scene.tscn"
+
+var player_at_entrance: bool = false
+
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("ui_select") and player_at_entrance:
+		get_tree().change_scene_to_file(next_scene)
+		
+func _on_body_entered(body: Node2D) -> void:
+	if body is Character:
+		player_at_entrance = true
+		
+func _on_body_exited(body: Node2D) -> void:
+	if body is Character:
+		player_at_entrance = false
