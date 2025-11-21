63f21a597c26f8ca17f4399477250f210f1d6af3
1haut <ozioma.marshall.anike@funkweb.org>
2025-11-21 13:29:31 +0100

    fix: karakter

--- noder/karakterer/scripts/karakter.gd
+++ noder/karakterer/scripts/karakter.gd
@@ -1,11 +1,2 @@
 extends CharacterBody2D
 class_name Character
-
-var speed: int = 50
-
-func _physics_process(delta: float) -> void:
-	var direction_vector: Vector2 = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
-	
-	velocity = direction_vector * speed
-
-	move_and_slide()
