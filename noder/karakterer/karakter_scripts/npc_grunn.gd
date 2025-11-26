67d689885ab835232e4675ae32df1f8823e86cdc
hanseei <32166248+hanseei@users.noreply.github.com>
2025-11-26 15:09:34 +0100

    Lagt inn animasjoner, kodet for retninger
    Satt inn spritesheets og animasjoner for alle syv dyrene og får animasjoner til å spille riktig basert på retning. Også laget en json-fil for informasjon for npcene, jobber med å hente dette i kode nå.

--- noder/karakterer/karakter_scripts/npc_grunn.gd
+++ noder/karakterer/karakter_scripts/npc_grunn.gd
@@ -1,25 +1,33 @@
 extends CharacterBody2D
 
+@export_enum("gris", "hone", "hund", "kanin", "katt", "rev", "sau") var npc_type: String = "gris"
+
 @export var npc_name: String = "Ukjent"
 @export var speed: float = 50.0
 
+var json_string: String = FileAccess.get_file_as_string("res://noder/karakterer/karakter_scripts/npc_info.json")
+var json_dict
 var direction: Vector2 = Vector2.ZERO
 var walk_time: float = 0.0
 var elapsed_time: float = 0.0
 
 func _ready():
+	json_dict = JSON.parse_string(json_string)
 	print("NPC klar:", npc_name)
-
+	if (json_dict):
+		print(json_dict)
 	if has_node("Label"):
 		$Label.text = npc_name
 
 	if has_node("AnimatedSprite2D"):
 		var sprite_node = $AnimatedSprite2D
-		var frames = load("res://assets/grafikk/spriteframes/gris_spriteframes.tres")
+		var loadPath = "res://assets/grafikk/sprite_resources/" + npc_type + "_animasjon.tres"
+		print(loadPath)
+		var frames = load(loadPath)
 
 		if frames:
 			sprite_node.frames = frames
-			sprite_node.animation = "idle"
+			sprite_node.animation = "default"
 			sprite_node.play()
 			global_position = get_viewport_rect().size / 2
 
@@ -51,3 +59,14 @@ func _choose_new_direction():
 
 	if randi() % 5 == 0:
 		direction = Vector2.ZERO
+		$AnimatedSprite2D.play("default")
+	if abs(direction.x) < abs(direction.y):
+		if direction.y > 0:
+			$AnimatedSprite2D.play("walk_down")
+		else:
+			$AnimatedSprite2D.play("walk_up")
+	elif abs(direction.x) > abs(direction.y):
+		if direction.x > 0:
+			$AnimatedSprite2D.play("walk_right")
+		else:
+			$AnimatedSprite2D.play("walk_left")
