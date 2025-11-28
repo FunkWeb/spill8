ae225b6fadc9cc5b9266f9f37ec81de53fee83f0
Daafft <gamerdennisa@gmail.com>
2025-11-28 10:20:08 +0100

    Update Kredittering.gd

--- noder/Kredittering/Kredittering.gd
+++ noder/Kredittering/Kredittering.gd
@@ -1,4 +1,4 @@
 extends Control
 
 func _on_tilbake_knapp_pressed():
-    get_tree().change_scene_to_file("res://scener/hovedmeny.tscn")
+	get_tree().change_scene_to_file("res://scener/hovedmeny.tscn")
