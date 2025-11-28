7c5a61a2c0cff2ef218dbb93c72c288318e3dc27
Dennis <gamerdennisa@gmail.com>
2025-11-28 05:58:27 +0000

    Feat/basic credits and button set up

--- noder/hovedmeny/hovedmeny.gd
+++ noder/hovedmeny/hovedmeny.gd
@@ -11,3 +11,7 @@ func _on_avslutt_pressed() -> void:
 
 func _on_eggsamling_pressed() -> void:
 	get_tree().change_scene_to_file("res://scener/egg_collection_scene.tscn")
+
+func _on_kredittering_knapp_pressed():
+    # Bytt til kreditteringsscenen
+    get_tree().change_scene_to_file("res://scener/kredittering/kredittering.tscn")
