cba2baf0f64af506aaccb7361619ff67be975a2a
hanseei <32166248+hanseei@users.noreply.github.com>
2025-12-01 13:12:43 +0100

    La til en 'E'-indikator når du kan interagere med ting

--- noder/bondegard/bondegard_scripts/minigame_prompt.gd
+++ noder/bondegard/bondegard_scripts/minigame_prompt.gd
@@ -13,9 +13,11 @@ func _on_body_entered(body: Node2D) -> void:
 		body.get_node("Camera2D/Dialogboks").text_box.text = spilltext
 		body.get_node("Camera2D/Dialogboks").name_box.text = spillnavn
 		body.get_node("Camera2D/Dialogboks").visible = true
+		$AnimatedSprite2D.visible = true
 	super(body)
 	
 func _on_body_exited(body: Node2D) -> void:
 	if body.name == "karakter":
 		body.get_node("Camera2D/Dialogboks").visible = false
+		$AnimatedSprite2D.visible = false
 	super(body)
